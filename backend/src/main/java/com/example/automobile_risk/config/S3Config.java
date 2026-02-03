4821f4a0c2990cefb50c22e63486697d5e7ea4c0
82User <zxcvc9999@gmail.com>
2026-02-03 09:40:05 +0900

    Revert "sure"
    This reverts commit d09cbf6301d0eb59199aae865df325661eaf4be3.

--- backend/src/main/java/com/example/automobile_risk/config/S3Config.java
+++ backend/src/main/java/com/example/automobile_risk/config/S3Config.java
@@ -1,5 +1,7 @@
 package com.example.automobile_risk.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,12 @@ import org.springframework.context.annotation.Profile;
 @Profile("prod")
 public class S3Config {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
@@ -18,6 +26,11 @@ public class S3Config {
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(accessKey, secretKey)
+                        )
+                )
                 .build();
     }
 }

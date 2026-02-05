69822c7a1b9b84844409490d0a5b4ca6adddff16
uiseongk <khyuna5509@gmail.com>
2026-02-05 17:20:14 +0900

    design: 회색 글씨 검은색으로 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,7 +49,7 @@ function ProtectedRoute({
   children: JSX.Element;
   isLoggedIn: boolean;
 }) {
-  if (!isLoggedIn) return <Navigate to="/login" replace />;
+  if (!isLoggedIn) return <Navigate to="/" replace />;
   return children;
 }
 
